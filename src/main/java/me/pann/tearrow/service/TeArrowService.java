7c5f82c822e5cd08bafdad1007c560fe4a39d1b8
pann <i@pann.me>
2026-06-17 17:15:19 +0900

    Add TeArrowService class for generating replies with video titles

---
+++ src/main/java/me/pann/tearrow/service/TeArrowService.java
@@ -0,0 +1,36 @@
+package me.pann.tearrow.service;
+
+import me.pann.tearrow.dearrow.DeArrowClient;
+import me.pann.tearrow.videoid.VideoIdExtractor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class TeArrowService {
+	private final DeArrowClient deArrowClient;
+
+	public TeArrowService(DeArrowClient deArrowClient) {
+		this.deArrowClient = deArrowClient;
+	}
+
+	public Optional<String> createReply(String message) {
+		List<String> videoIds = VideoIdExtractor.extractVideoIds(message);
+		List<String> titles = new ArrayList<>();
+		for (String videoId : videoIds) {
+			Optional<String> title = deArrowClient.findTitle(videoId);
+			title.ifPresent(titles::add);
+		}
+
+		if (!titles.isEmpty()) {
+			StringBuilder replyBuilder = new StringBuilder("DeArrow:\n");
+			for (String title : titles) {
+				replyBuilder.append(title).append("\n");
+			}
+			replyBuilder.setLength(replyBuilder.length() - 1);
+			return Optional.of(replyBuilder.toString());
+		} else {
+			return Optional.empty();
+		}
+	}
+}
